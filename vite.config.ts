0d5b8464105bd25a3dab0f589216a637c3a1efbd
RehanSportskeeda <93132708+RehanSportskeeda@users.noreply.github.com>
2025-09-15 17:09:02 +0530

    Configure Vite's React plugin to process SVG imports with svgr

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,9 @@ import { resolve } from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/games/tennis-duo/',
-  plugins: [react()],
+  plugins: [react({
+    svgr: true
+  })],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
